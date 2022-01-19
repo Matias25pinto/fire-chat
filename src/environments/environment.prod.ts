e6c20ca156ebcbcdd50fd743f118e746f766a866
Matias Pinto <matias25pinto@gmail.com>
2022-01-19 17:55:35 -0300

    ADD(environment):Se agrego los keys de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAFIL7lV22V-K6Si4IkFW4OCE4S1HiNBkg',
+    authDomain: 'firechat-a98f0.firebaseapp.com',
+    databaseURL: 'https://firechat-a98f0.firebaseio.com',
+    projectId: 'firechat-a98f0',
+    storageBucket: 'firechat-a98f0.appspot.com',
+    messagingSenderId: '1055313480358',
+    appId: '1:1055313480358:web:923e14d6c37c23ddca0267',
+    measurementId: 'G-DZ0HW1GS3N',
+  },
 };
